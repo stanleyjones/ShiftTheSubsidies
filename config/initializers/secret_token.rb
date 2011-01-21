2760d1915a2a7da4450941e0483425dc555b0470
Stanley Jones <stanley@diligentcreative.com>
2011-01-20 18:25:41 -0800

    Full App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShiftTheSubsidies::Application.config.secret_token = 'dd9ea09cdeb6d54bb52c8266bfa28aeb2476e9011b88f54ebfeb81bd855cd0f5aad5bee84d577f42f65ed358e08209f0b309bb111d2a093f2ae09d9874d6d15b'
