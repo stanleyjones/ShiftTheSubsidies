725db1d5a702fe5d5404306144eecefe95ed93df
Stanley Jones <stanley@diligentcreative.com>
2012-09-06 13:27:36 -0700

    More syntax changes for Money 5.0.0

--- app/helpers/subsidies_helper.rb
+++ app/helpers/subsidies_helper.rb
@@ -27,10 +27,10 @@ module SubsidiesHelper
     Money::Currency.register(uac)
 
 	def currency_options_for_select( selected = nil )
-		currencies = Money::Currency::TABLE.keys
+		currencies = Money::Currency.table.keys
 		currency_options = []
 		currencies.collect { |c|
-			currency = Money::Currency::TABLE[c]
+			currency = Money::Currency.table[c]
 			currency_options << ["#{currency[:name]} (#{currency[:iso_code]})", currency[:iso_code]]
 		}
 		#currencies.sort!
