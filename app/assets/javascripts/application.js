7c16f1e1d4325706ca3c5706115568142132757b
Stanley Jones <stanley@diligentcreative.com>
2013-05-14 10:38:18 -0700

    Changed all visualizations to use latest libraries.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,9 @@
-//= require jquery-1.9.1.js
-//= require jquery.dataTables-1.9.4.js
+//= require jquery-1.9.1.min.js
+//= require jquery.dataTables-1.9.4.min.js
 
-//= require d3.v3.js
+//= require d3.v3.min.js
+
+//= require jquery.vector-map-0.1.min.js
+//= require jquery.vector-map.world.js
 
 //= require main.js
