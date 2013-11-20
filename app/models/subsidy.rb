31aad39939234c8afa04928ffdeb047f43b56d28
Stanley Jones <stanley@diligentcreative.com>
2013-11-20 12:02:55 -0800

    Tabs, not spaces.

--- app/models/subsidy.rb
+++ app/models/subsidy.rb
@@ -5,56 +5,56 @@ class Subsidy < ActiveRecord::Base
 	validates :amount_usd, :numericality => { :greater_than_or_equal_to => 0 }, :allow_nil => true
 	validates :amount_original, :numericality => { :greater_than_or_equal_to => 0 }
 	validates :currency, :presence => true
-  validates :exchange_rate, :presence => true, :numericality => true
+	validates :exchange_rate, :presence => true, :numericality => true
 	validates :institution, :presence => true
 	validates :entity, :presence => true
 	validates :project, :presence => true
 	validates :kind, :inclusion => {:in => ['Equity','Grant','Guarantee','Loan']}
 
-  belongs_to :institution, :touch => true
-  belongs_to :entity, :touch => true
-  belongs_to :project, :touch => true
+	belongs_to :institution, :touch => true
+	belongs_to :entity, :touch => true
+	belongs_to :project, :touch => true
 
-  has_one :sector, :through => :project
-  has_one :region, :through => :project
+	has_one :sector, :through => :project
+	has_one :region, :through => :project
 
-  attr_accessible :amount_original, :currency, :amount_usd, :date, :institution_id, :entity_id, :project_id, :kind, :approved, :source
-  
-  def self.live
-  	# TODO: Improve the math for fiscal years
-  	Subsidy.joins(:institution).where(
-      "institutions.visible = true AND approved = true AND date > :start_date AND date < :end_date AND amount_original > 0",
-  		{:start_date => "#{START_YEAR-1}-01-01", :end_date => "#{END_YEAR}-12-31"}
-  	).uniq
-  end
-  
-  def amount
-  	Rails.cache.fetch("subsidy/#{self.id}-#{self.updated_at}/amount") do
-  		if self.amount_usd
-  			self.amount_usd
-  		elsif self.amount_original
-	  		if self.currency == "USD"
-  				return self.amount_original
-        elsif self.exchange_rate != nil
-          return self.amount_original * self.exchange_rate * 1.0
-        else
-          return 0
-	  		end
-  		else
-  			return 0
-  		end
-  	end
-  end
-  
-  def in_range?(start_date,end_date)
-  	if self.date 
-  		return (self.date >= start_date and self.date <= end_date)
-  	else
-  		false
-  	end
-  end
-  
-  def in_category?(category)
+	attr_accessible :amount_original, :currency, :amount_usd, :date, :institution_id, :entity_id, :project_id, :kind, :approved, :source
+	
+	def self.live
+		# TODO: Improve the math for fiscal years
+		Subsidy.joins(:institution).where(
+			"institutions.visible = true AND approved = true AND date > :start_date AND date < :end_date AND amount_original > 0",
+			{:start_date => "#{START_YEAR-1}-01-01", :end_date => "#{END_YEAR}-12-31"}
+		).uniq
+	end
+	
+	def amount
+		Rails.cache.fetch("subsidy/#{self.id}-#{self.updated_at}/amount") do
+			if self.amount_usd
+				self.amount_usd
+			elsif self.amount_original
+				if self.currency == "USD"
+					return self.amount_original
+				elsif self.exchange_rate != nil
+					return self.amount_original * self.exchange_rate * 1.0
+				else
+					return 0
+				end
+			else
+				return 0
+			end
+		end
+	end
+	
+	def in_range?(start_date,end_date)
+		if self.date 
+			return (self.date >= start_date and self.date <= end_date)
+		else
+			false
+		end
+	end
+	
+	def in_category?(category)
 		if self.project and self.project.sector and self.project.sector.category
 			return self.project.sector.category == category
 		else
@@ -62,8 +62,8 @@ class Subsidy < ActiveRecord::Base
 		end
 	end
 
-  private
-  
+	private
+	
 	def update_amount_usd
 		usd = self.amount_usd # Keep the original value safe!
 		if self.amount_original and self.currency == "USD"
