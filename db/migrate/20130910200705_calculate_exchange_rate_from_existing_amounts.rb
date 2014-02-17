15c762db79c2922e3473bed8fc4306a9fa98321c
Stanley Jones <stanley@diligentcreative.com>
2014-02-16 23:06:22 -0800

    Removed redundant migrations.

--- db/migrate/20130910200705_calculate_exchange_rate_from_existing_amounts.rb
+++ db/migrate/20130910200705_calculate_exchange_rate_from_existing_amounts.rb
@@ -1,11 +1,17 @@
 class CalculateExchangeRateFromExistingAmounts < ActiveRecord::Migration
-  def up
-  	change_column :subsidies, :exchange_rate, :decimal, :precision => 10, :scale => 5
-  	Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=(`amount_usd` / `amount_original`)')
-  end
+	def up
+		change_column :subsidies, :exchange_rate, :decimal, :precision => 10, :scale => 5
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=(`amount_usd` / `amount_original`)')
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=(`amount_usd` / `amount_original`)')
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=1.32 WHERE `currency` like "%EUR%" AND `date` LIKE "%2012%"')
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=1.30 WHERE `currency` like "%EUR%" AND `date` LIKE "%2011%"')
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=1.34 WHERE `currency` like "%EUR%" AND `date` LIKE "%2010%"')
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=1.43 WHERE `currency` like "%EUR%" AND `date` LIKE "%2009%"')
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=1.39 WHERE `currency` like "%EUR%" AND `date` LIKE "%2008%"')
+	end
 
-  def down
-  	change_column :subsidies, :exchange_rate, :decimal
-  	Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=1')
-  end
+	def down
+		change_column :subsidies, :exchange_rate, :decimal
+		Subsidy.connection.execute('UPDATE `subsidies` SET `exchange_rate`=1')
+	end
 end
--- db/migrate/20131113192653_add_exchange_rate_to_subsidies.rb
+++
@@ -1,8 +0,0 @@
-class AddExchangeRateToSubsidies < ActiveRecord::Migration
-  def self.up
-  	add_column :subsidies, :exchange_rate, :decimal, :default => 1.0
-  end
-  def self.down
-  	remove_column :subsidies, :exchange_rate
-  end
-end
